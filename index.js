81526a498fbaa42e08d48f60d476af5ff0e5dd7b
jinhongyang <930334030@qq.com>
2018-02-13 20:29:36 +0800

    add config page

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // 引入electron模块
-let electron = require('electron');
-let bilibliLive= require('./app/app');
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+const bilibliLive= require('./app/app');
 // 用来控制应用的生命周期
 let app = electron.app;
-
+let ipcMain  = electron.ipcMain;
 
 // 用来创建浏览器窗口
 let BrowserWindow = electron.BrowserWindow;
@@ -11,10 +13,33 @@ let BrowserWindow = electron.BrowserWindow;
 
 // 创建一个全局变量，相当于普通网页中的 window 对象
 let mainWindow;
+let appWindow;
 
-
-// 当软件触发 ready 事件之后，创建浏览器窗口
+// 当软件触发 ready 事件之后，创建浏览器窗口ßß
 app.on('ready', function() {
+    appWindow = new BrowserWindow({
+        width: 300,
+        height:180,
+        title:'Config'
+    });
+    appWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'config.html'),
+        protocol: 'file:',
+        slashes:true
+    }));
+    // Handle garbage collection
+    appWindow.on('close', function(){
+        appWindow = null;
+    });
+    //appWindow.toggleDevTools();
+});
+
+ipcMain.on('bilibili:add',function (e,roomid) {
+    createAppWindow(roomid);
+    appWindow.close();
+});
+
+function createAppWindow(roomid){
 
     // 创建浏览器窗口。
     mainWindow = new BrowserWindow({
@@ -28,13 +53,18 @@ app.on('ready', function() {
         frame: false,
 
     });
-    bilibliLive(388,mainWindow);
+    bilibliLive(roomid,mainWindow);
     //mainWindow.toggleDevTools();
 
     // 加载 example 目录下的index.html
-    mainWindow.loadURL('file://'+__dirname+'/index.html');
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes:true
+    }));
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
-});
 
+
+}
